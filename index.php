a36d5bd79b270b57add29cd7095691e19a4e6cd3
HammamAlfarizi07 <hammamalvariz7118@gmail.com>
2023-05-24 14:56:52 +0700

    menambkan file index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/dist/output.css">
+    <script src="https://kit.fontawesome.com/6f0d2c27b1.js" crossorigin="anonymous"></script>
+    <title>Coba git remote</title>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet </p>
+</body>
+</html>
